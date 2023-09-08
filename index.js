172376c84c1662e018c584c7e69b82fa5c3fb9af
Raj Maurya <89681751+rajmaurya2611@users.noreply.github.com>
2023-09-08 15:29:12 +0530

    NPM Modules

---
+++ index.js
@@ -0,0 +1,16 @@
+//var generateName = require('sillyname');
+
+//import generateName from sillyName;
+//var sillyName = generateName();
+
+//console.log(`My Name is ${sillyName}.`);
+
+//const superheroes = require('superheroes');
+import superheroes from 'superheroes'; 
+superheroes.all;
+//=> ['3-D Man', 'A-Bomb', …]
+ 
+const name = superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(`My name is Raj and I am ${name}.`);
